157ed8f6107efca3142169c3181009611dfb10bc
cranberrysoft <dubielecki.mariusz@gmail.com>
2024-12-17 22:15:41 +1100

    Day 1 - Simple naive solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val a = mutableListOf<Int>()
+        val b = mutableListOf<Int>()
+        for (line in input) {
+            val split = line.split(Regex("\\s+"))
+            a.add(split[0].toInt())
+            b.add(split[1].toInt())
+        }
+        a.sort()
+        b.sort()
+
+        var result = 0
+        a.forEachIndexed { index, i -> result += Math.abs(i - b[index]) }
+
+        return result
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(part1(testInput))
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
 }
