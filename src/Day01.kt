53f5b7798d2e02bfa6d3996a2e6efa2847c5c544
cranberrysoft <dubielecki.mariusz@gmail.com>
2024-12-18 21:01:01 +1100

    Day 1 - Simple naive solution - part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,3 +1,5 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
         val a = mutableListOf<Int>()
@@ -11,16 +13,32 @@ fun main() {
         b.sort()
 
         var result = 0
-        a.forEachIndexed { index, i -> result += Math.abs(i - b[index]) }
+        a.forEachIndexed { index, i -> result += abs(i - b[index]) }
 
+        return result
+    }
+
+    fun part2(input: List<String>): Int {
+        val a = mutableListOf<Int>()
+        val b = mutableMapOf<Int, Int>()
+        for (line in input) {
+            val split = line.split(Regex("\\s+"))
+            a.add(split[0].toInt())
+            b.compute(split[1].toInt()) { _, v -> v?.plus(1 ) ?: 1 }
+        }
+
+        var result = 0
+        a.forEach {i -> result+= i * b.getOrDefault(i, 0) }
         return result
     }
 
     val testInput = readInput("Day01_test")
     println(part1(testInput))
+    println(part2(testInput))
     check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
+    part2(input).println()
 }
